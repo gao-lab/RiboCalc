a7295b81c69217582af186517040a7514ed0e4a4
kkkyj <lybird300@gmail.com>
2021-09-05 20:31:41 +0800

    add classification model data

--- classification_models/script/run.sh
+++ classification_models/script/run.sh
@@ -1,8 +1,23 @@
-# s1
-~/bin/Mutilanalysis/plsregress  -t category -c1v2 --nlvs=10 --nlvs-max=10 feat.tab -s sample1.dat.random.sel --nlvs-optimize
+## This script is to build PLS classification model to assess characteristict of coding/CDCT/noncoding transcripts
 
-# s2
-~/bin/Mutilanalysis/plsregress  -t category -c1v2 --nlvs=6 --nlvs-max=6  feat.tab  -s sample1.dat.random.sel --mcuve --mc-nlvs=6
+# Dependencies:
+# Pliease install the following packages
+# https://github.com/zju3351689/rklib
+# https://github.com/zju3351689/rblib
 
-# s3
-~/bin/Mutilanalysis/plsregress  -t category -c1v2 --nlvs=6 --nlvs-max=6  feat.tab  -s sample1.dat.random.sel --ex_predict --ex_sampleinfo sample1.dat.nosel --ex_matrix dat.nosel.T
+
+## We build two models
+# coding vs noncoding
+myprefix="../feature_data/coding_noncoding"
+# coding vs CDCT
+myprefix="../feature_data/coding_CDCT"
+
+
+# step 1 number of latent varialbles optimazing
+~/bin/Mutilanalysis/plsregress -t category -c1v2 --nlvs=10 --nlvs-max=10 $myprefix.train.feat -s $myprefix.train.info --nlvs-optimize
+
+# step 2 feature importance assessment
+~/bin/Mutilanalysis/plsregress -t category -c1v2 --nlvs=6 --nlvs-max=6 $myprefix.train.feat -s $myprefix.train.info --mcuve --mc-nlvs=6
+
+# step 3 independent testing
+~/bin/Mutilanalysis/plsregress -t category -c1v2 --nlvs=6 --nlvs-max=6 $myprefix.train.feat -s $myprefix.train.info --ex_predict --ex_sampleinfo $myprefix.test.info --ex_matrix $myprefix.test.feat
