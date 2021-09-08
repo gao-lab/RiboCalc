8bcebb5a1677cee0c2aa687abd5ee9357c6eb3a9
kkkyj <33247875+kkkyj@users.noreply.github.com>
2021-09-08 14:24:46 +0800

    Update run.sh

--- classification_models/script/run.sh
+++ classification_models/script/run.sh
@@ -14,10 +14,10 @@ myprefix="../feature_data/coding_CDCT"
 
 
 # step 1 number of latent varialbles optimazing
-~/bin/Mutilanalysis/plsregress -t category -c1v2 --nlvs=10 --nlvs-max=10 $myprefix.train.feat -s $myprefix.train.info --nlvs-optimize
+plsregress -t category -c1v2 --nlvs=10 --nlvs-max=10 $myprefix.train.feat -s $myprefix.train.info --nlvs-optimize
 
 # step 2 feature importance assessment
-~/bin/Mutilanalysis/plsregress -t category -c1v2 --nlvs=6 --nlvs-max=6 $myprefix.train.feat -s $myprefix.train.info --mcuve --mc-nlvs=6
+plsregress -t category -c1v2 --nlvs=6 --nlvs-max=6 $myprefix.train.feat -s $myprefix.train.info --mcuve --mc-nlvs=6
 
 # step 3 independent testing
-~/bin/Mutilanalysis/plsregress -t category -c1v2 --nlvs=6 --nlvs-max=6 $myprefix.train.feat -s $myprefix.train.info --ex_predict --ex_sampleinfo $myprefix.test.info --ex_matrix $myprefix.test.feat
+plsregress -t category -c1v2 --nlvs=6 --nlvs-max=6 $myprefix.train.feat -s $myprefix.train.info --ex_predict --ex_sampleinfo $myprefix.test.info --ex_matrix $myprefix.test.feat
